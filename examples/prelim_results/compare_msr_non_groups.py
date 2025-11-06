b89f11b275398b5254ade2bfb2c8788a59db4220
LukeSeifert <lukeseifert23@gmail.com>
2025-11-06 09:43:09 -0600

    Account for overflow

--- examples/prelim_results/compare_msr_non_groups.py
+++ examples/prelim_results/compare_msr_non_groups.py
@@ -5,10 +5,14 @@ import time
 
 def msr_group(T, t, tin, tex, f_rate, yields, halflives):
     tnet = tin+tex
-    J = int(np.floor(T/tnet))
-    Jin = int(np.floor((T-tin)/tnet))
-    full_sum = 0
+    try:
+        J = int(np.floor(T/tnet))
+        Jin = int(np.floor((T-tin)/tnet))
+    except OverflowError:
+        J = 0
+        Jin = 0
     j_sum = 0
+    full_sum = 0
     for k in range(len(yields)):
         lam = np.log(2) / halflives[k]
         nu = yields[k]
@@ -63,20 +67,20 @@ tin = 10
 tex = 0
 tnet = tin+tex
 halflives = [1]#[100]
-yields = [1]#[0.00693]
+yields = [0.00693]
 f_rate = 1
-min_tin = 5
-min_tex = 5
-max_tin = 7
-max_tex = 7
-num_nodes = 200
+min_tin = 8
+min_tex = 8
+max_tin = 10
+max_tex = 10
+num_nodes = 400
 tins = np.linspace(min_tin, max_tin, num_nodes)
 texs = np.linspace(min_tex, max_tex, num_nodes)
 J = int(np.floor(T/tnet))
 Jin = int(np.floor((T-tin)/tnet))
 print(f'{J = }\n{Jin = }')
 
-plot_t = True
+plot_t = False
 plot_hm = not plot_t
 
 
