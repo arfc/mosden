Fix fission rate calculation for non-zero excore time

mosden/concentrations.py
@@ -219,9 +219,13 @@ class Concentrations(BaseClass):
             self._collect_omc_nuyield()
         return data
     
-    def read_omc_fission_json(self) -> tuple[dict[str, np.ndarray], np.ndarray]:
+    def read_omc_fission_json(self, only_incore:bool = False) -> tuple[dict[str, np.ndarray], np.ndarray]:
         """
         Collect the fission rate history from a json file
+
+        only_incore : bool
+            Return the fission rate history ONLY for the in-core residence times
+            (this assumes starting in core and then alternates)
 
         Returns
         -------
@@ -236,6 +240,8 @@ class Concentrations(BaseClass):
         fissions = full_data['fissions']
         times = np.array(full_data['times'])
         for nuc in fissions.keys():
+            if only_incore:
+                fissions[nuc] = fissions[nuc][::2]
             fissions[nuc] = np.array(fissions[nuc])
         return fissions, times
     
@@ -246,9 +252,7 @@ class Concentrations(BaseClass):
             for nuc in full_data[type_yield].keys():
                 full_data[type_yield][nuc] = np.array(full_data[type_yield][nuc])
         return full_data
-
-
-    
+ 
     def _collect_omc_fissions(self) -> tuple[dict[str, np.ndarray], np.ndarray]:
         """
         Collect the fission rate history from OpenMC output and writes to a

mosden/groupfit.py
@@ -59,7 +59,7 @@ class Grouper(BaseClass):
         times = None
 
         if self.omc:
-            fission_term, times = conc_handler.read_omc_fission_json()
+            fission_term, times = conc_handler.read_omc_fission_json(only_incore=True)
             fission_term = fission_term['net']
 
         if self.irrad_type == 'pulse':
@@ -243,8 +243,8 @@ class Grouper(BaseClass):
                     refined_term.append(self.fission_term[i])
                     break
         self.refined_fission_term = np.asarray(refined_term)
-        self.logger.info('New derivation required for time dependent fission rate history')
-        self.refined_fission_term = np.mean(self.refined_fission_term)
+        self.logger.debug('Time dependent fission rate history not enabled')
+        self.refined_fission_term = np.mean(self.fission_term)
         return self.refined_fission_term
 
     def _nonlinear_least_squares(self,