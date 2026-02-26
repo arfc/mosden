0796497904bc25b74847adfd2482b7924771e9f8
LukeSeifert <lukeseifert23@gmail.com>
2026-02-26 11:28:40 -0600

    Fix schema and update appendixD analysis

---
+++ examples/prelim_results/appendixD/indepth_analysis.py
@@ -0,0 +1,10 @@
+
+
+
+
+if __name__ == '__main__':
+    irrad_selection = 0
+    data_selection = 0
+    irrad_types = ['pulse', 'saturation', 'res-combined', 'long-short']
+    data_types = ['post-irrad', 'all', 'incore']
+
