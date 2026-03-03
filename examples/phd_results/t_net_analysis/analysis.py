4f30d8ae94762e13cdbdef4c7d891c8cee6fa6a7
LukeSeifert <lukeseifert23@gmail.com>
2026-03-03 14:57:42 -0600

    Move data and update analysis

--- examples/phd_results/t_net_analysis/analysis.py
+++ examples/phd_results/t_net_analysis/analysis.py
@@ -5,12 +5,13 @@ import glob
 import os
 plt.style.use('mosden.plotting')
 
-def plot_data(data_vals):
+def plot_data(data_vals, namemod=''):
     formatted_data = defaultdict(list)
     formatted_data['yields'] = defaultdict(list)
     formatted_data['hls'] = defaultdict(list)
     formatted_data["xs"] = []
     xlab = 'Irradiation Time [s]'
+    xscale = 'log'
 
     for t_net, params in data_vals.items():
         formatted_data['xs'].append(t_net)
@@ -28,12 +29,13 @@ def plot_data(data_vals):
                     linewidth=1)
         plt.legend()
         plt.xlabel(xlab)
+        plt.xscale(xscale)
         if name == 'yields':
             ylab = 'Group Yield'
         elif name == 'hls':
             ylab = 'Group Half-life [s]'
         plt.ylabel(ylab)
-        plt.savefig(f'{name}.png')
+        plt.savefig(f'{name}{namemod}.png')
         plt.close()
     
     xs = formatted_data['xs']
@@ -45,20 +47,21 @@ def plot_data(data_vals):
     plt.stackplot(xs, y_arrays, labels=labels)
 
     plt.xlabel(xlab)
+    plt.xscale(xscale)
     plt.ylabel('Yield')
     plt.legend(loc='upper left')
 
     plt.tight_layout()
-    plt.savefig('stack_yields.png')
+    plt.savefig(f'stack_yields{namemod}.png')
     plt.close()
 
-def build_data_dict(data_path=r'./'):
+def build_data_dict(data_path=r'./data/'):
     def helper(pathmod):
         files = glob.glob(os.path.join(data_path, f"*{pathmod}.csv"))
         data = {}
         for file in files:
             file: str = file
-            time = file.split('_')[1]
+            time = float(file.split('_')[1])
             data[time] = dict()
             file_data = CSVHandler(file).read_vector_csv()
             data[time]['yields'] = file_data['yield']
@@ -73,4 +76,5 @@ def build_data_dict(data_path=r'./'):
 
 if __name__ == '__main__':
     post_data, all_data = build_data_dict()
-    plot_data(post_data)
+    plot_data(post_data, '_post')
+    plot_data(all_data, '_all')
