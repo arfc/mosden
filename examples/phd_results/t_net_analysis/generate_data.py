1d3f9af57adb08c40cfaccae284ebfe73478e874
LukeSeifert <lukeseifert23@gmail.com>
2026-03-17 15:25:31 -0500

    Add data generation script

---
+++ examples/phd_results/t_net_analysis/generate_data.py
@@ -0,0 +1,30 @@
+import subprocess
+import json
+import shutil
+
+
+
+if __name__ == "__main__":
+    num_groups = [4, 6, 8, 10, 12]
+    irrad_times = [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4]
+    for group in num_groups:
+        output_dir = f'./dataNet_{group}'
+        input_file = './input.json'
+        for T in irrad_times:
+            with open(input_file, 'r') as f:
+                data = json.load(f)
+            eval_method = 'post-irrad'
+            data['modeling_options']['residual_handling'] = [eval_method] 
+            data['modeling_options']['incore_s'] = T
+            data['modeling_options']['net_irrad_s'] = T
+            with open(input_file, 'w') as f:
+                json.dump(data, f, indent=4)
+
+            subprocess.run(['mosden', '-pre', input_file])
+            subprocess.run(['mosden', '-m', input_file])
+            shutil.move('./group_parameters.csv', f'{output_dir}/intermediate_{T}_{eval_method}.csv')
+            subprocess.run(['mosden', '-g', input_file])
+
+            eval_method = 'all'
+            data['modeling_options']['residual_handling'] = [eval_method] 
+            shutil.move('./group_parameters.csv', f'{output_dir}/intermediate_{T}_{eval_method}.csv')
