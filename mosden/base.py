Correct off by one error and update avg MeV to include obj
Co-authored-by: Copilot <copilot@github.com>

mosden/base.py
@@ -505,26 +505,27 @@ class BaseClass:
         data = csv_handler.read_csv()
         return data
     
-    def calculate_avg_MeV(self, MeV_bins: list[float],
-                           probabilities: list[float]) -> float:
+    def calculate_avg_MeV(self, MeV_bins: list[float|object],
+                           probabilities: list[float|object]) -> float|object:
         """
         Calculate the average energy from `self.energy_groups_MeV` and the 
-        given probabilities for each bin
+        given probabilities for each bin.
+        Useable with both floats and unumpy arrays of ufloats.
 
         Parameters
         ----------
-        MeV_bins : list[float]
+        MeV_bins : list[float|object]
             The energy bins
 
-        probabilities : list[float]
+        probabilities : list[float|object]
             Probability for each bin
 
         Returns
         -------
-        avg_e : float
+        avg_e : float|object
             Average energy (MeV)
         """
-        normalized_probs = probabilities / np.sum(probabilities)
+        normalized_probs = probabilities / sum(probabilities)
         midpoints_MeV = np.asarray(self._get_midpoint_eVs(MeV_bins)) * 1e-6
         energies = [midpoints_MeV[i]*normalized_probs[i] for i in range(len(normalized_probs))]
         avg_e = sum(sorted(energies))

mosden/postprocessing.py
@@ -1078,6 +1078,21 @@ class PostProcess(BaseClass):
             plt.close() 
         return None
     
+    def _log_average_energies(self, spectra_params: np.ndarray[np.ndarray[object]]) -> None:
+        """
+        Use `self.logger.info` to record the average energy of each group from
+        the calcualted spectra_params that include uncertainties
+
+        Parameters
+        ----------
+        spectra_params : np.ndarray[np.ndarray[object]]
+            Array for each group containing unumpy array of ufloats for each
+            energy bin, providing params with uncertainties
+        """
+        for group in range(self.num_groups):
+            avg_MeV = self.calculate_avg_MeV(self.energy_groups_MeV, spectra_params[group])
+            self.logger.info(f'Group {group+1} Average Energy: {avg_MeV*1000:.3f} keV')
+        return None
 
     def _get_group_spectra_sigma(self) -> np.ndarray[np.ndarray[object]]:
         """
@@ -1088,7 +1103,7 @@ class PostProcess(BaseClass):
 
         Returns
         -------
-        np.ndarray[unumpy.uarray[object]]
+        group_spectra : np.ndarray[unumpy.uarray[object]]
             Array for each group containing unumpy array of ufloats for each
             energy bin, providing params with uncertainties
         """
@@ -1097,7 +1112,7 @@ class PostProcess(BaseClass):
         else:
             raise ValueError("No MC spectra data available")
         group_spectra = np.zeros((self.num_groups,
-                                  len(self.energy_groups_MeV)), dtype=object)
+                                  len(self.energy_groups_MeV[:-1])), dtype=object)
         for group in range(self.num_groups):
             for ei in range(len(self.energy_groups_MeV[:-1])):
                 bin_samples = [spectra[MC_i][group][ei] for MC_i in range(self.MC_samples)]